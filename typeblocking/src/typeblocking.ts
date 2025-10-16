b76ba9ff8a0460ee7717b913624ac909fa8d32e8
Michael Gallagher <mjgallag@gmail.com>
2025-10-15 19:03:10 -0700

    Register shift+key (e.g. ")

--- typeblocking/src/typeblocking.ts
+++ typeblocking/src/typeblocking.ts
@@ -163,11 +163,17 @@ export class TypeBlocking {
             true,
         );
 
-        const SKIP = new Set([8, 13, 46]); // 8 = Backspace, 13 = Enter, 46 = Delete
+        const SKIP = new Set([8, 13, 16, 46]); // 8 = Backspace, 13 = Enter, 16 = Shift, 46 = Delete
         // NOTE: Plain keys (no modifiers)
         for (let code = 0; code <= 222; ++code) {
             if (SKIP.has(code)) continue;
             Blockly.ShortcutRegistry.registry.addKeyMapping(code, SHORTCUT, true);
+            // Register shift+key (e.g. ")
+            Blockly.ShortcutRegistry.registry.addKeyMapping(
+                Blockly.ShortcutRegistry.registry.createSerializedKey(code, [Blockly.utils.KeyCodes.SHIFT]),
+                SHORTCUT,
+                true
+            );
         }
     }
 }
